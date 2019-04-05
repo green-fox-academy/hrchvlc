6afc02c4d4c809ae06ea26377d894fbf5d0e9de5
Lucia Hrachová <hrachovalucia@gmail.com>
2019-04-05 15:28:06 +0200

    new exercises

--- week-01/day-4/src/drawDiagonal.java
+++ week-01/day-4/src/drawDiagonal.java
@@ -1,6 +1,6 @@
 public class drawDiagonal {
     public static void main(String[] args) {
-
+    
 
     }
 }
--- week-01/day-4/src/drawTriangle.java
+++ week-01/day-4/src/drawTriangle.java
@@ -6,10 +6,22 @@ public class drawTriangle {
 
         System.out.println("Give me a number!");
         int number = scanner.nextInt();
+        String star = "*";
 
+        int numberOfLine = 1;
+        String line = "";
+
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < numberOfLine ; j++) {
+                line += star;
+                System.out.println(line);
+            }
+        }
+        numberOfLine++;
 
     }
 }
+
 
 // Write a program that reads a number from the standard input, then draws a
 // triangle like this:

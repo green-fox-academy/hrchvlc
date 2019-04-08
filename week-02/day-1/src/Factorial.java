bf4b31b376a451907e9dd73e4a66788d265b84a8
Lucia Hrachová <hrachovalucia@gmail.com>
2019-04-08 15:24:55 +0200

    factorial

---
+++ week-02/day-1/src/Factorial.java
@@ -0,0 +1,21 @@
+public class Factorial {
+
+    public static int factorio(int endNumber) {
+        int factorio = 1;
+        for (int i = 1; i <= endNumber; i++) {
+            factorio = factorio * i;
+        } return factorio;
+    }
+
+    public static void main(String[]args) {
+        System.out.println(factorio(5));
+    }
+
+}
+
+
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a function called `factorio`
+//   that returns it's input's factorial

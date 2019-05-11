b3d002909b31f1533effe44f066345c008be26e3
Lucia Hrachová <hrachovalucia@gmail.com>
2019-05-11 16:26:41 +0200

    Matchmaking, ProductDatabase1 finished

--- week-02/day-2/src/Matchmaking.java
+++ week-02/day-2/src/Matchmaking.java
@@ -8,10 +8,24 @@ public class Matchmaking{
         // Write a method that joins the two lists by matching one girl with one boy into a new list
         // If someone has no pair, he/she should be the element of the list too
         // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+        //works, but might have better solution
 
-  //      System.out.println(makingMatches(girls, boys));
+        System.out.println(makingMatches(girls, boys));
     }
-  //  public static ArrayList makingMatches(List list, List secondList) {
-
-  //  }
+    public static ArrayList<String> makingMatches(List<String> girls, List<String> boys) {
+        ArrayList<String> pairedCouples = new ArrayList<>();
+        int boyIndex = 1;
+        for (String girl: girls) {
+            pairedCouples.add(girl);
+        }
+        for (String boy: boys) {
+            pairedCouples.add(boyIndex, boy);
+            if (pairedCouples.size() <= 9) {
+                boyIndex = boyIndex + 2;
+            } else {
+                boyIndex++;
+            }
+        }
+        return pairedCouples;
+    }
 }
--- week-02/day-2/src/ProductDatabase1.java
+++ week-02/day-2/src/ProductDatabase1.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,10 +14,40 @@ public class ProductDatabase1 {
 
         System.out.println("Fish is: " + products.get("Fish"));
 
-        int number = 0;
-        for (Map.Entry<String, Integer> item: products.entrySet()) {
-      //      number = products.values();
+        int price = 0;
+        for (int item: products.values()) {
+           if (price < item) {
+            price = item;
+           }
         }
+       for (Map.Entry<String, Integer> item: products.entrySet()) {
+           if (item.getValue() == price) {
+               System.out.println("Most expensive: " + price + ",it is a fucking " + item.getKey());
+           }
+       }
+       int average = 0;
+        for (Map.Entry<String, Integer> item: products.entrySet()) {
+            average += item.getValue();
+        }
+        average = average / products.size();
+        System.out.println("Average: " + average);
+
+        int count = 0;
+        for (Map.Entry<String, Integer> item: products.entrySet()) {
+            if (item.getValue() < 300) {
+                count++;
+            }
+        } System.out.println("How many below 300? " + count);
+
+        if (products.containsValue(125)) {
+            System.out.println("You can buy something with it");
+        } else {
+            System.out.println("Nah, nothing exact here.");
+        }
+
+        System.out.println("Cheapest: " + Collections.min(products.values()));
+
+
     }
 }
 // https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/data-structures/product-database/README.md

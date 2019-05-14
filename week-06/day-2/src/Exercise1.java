ca6f4787775db04f58c5d697b2dab197c3325aa2
Lucia Hrachová <hrachovalucia@gmail.com>
2019-05-14 16:17:09 +0200

    StreamAndLambda: things moved, things created and things deleted

--- week-06/day-2/src/Exercise1.java
+++ week-06/day-2/src/Exercise1.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class exercise1 {
+public class Exercise1 {
     public static void main(String[] args) {
         // Write a Stream Expression to get the even numbers
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
--- week-06/day-2/src/Exercise2.java
+++ week-06/day-2/src/Exercise2.java
@@ -7,7 +7,9 @@ public class Exercise2 {
         // Write a Stream Expression to get the squared value of the positive numbers
         List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
 
-        List<Integer> myList = numbers.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
-        myList.stream().forEach(i -> i * 2);
+        List<Integer> myList = numbers.stream().filter(i -> i > 0)
+                .map(i -> (int) Math.pow(i, 2))
+                .collect(Collectors.toList());
+        System.out.println(myList);
     }
 }
